e024e932b6651d7291eb0efd2c7eaed4d7fa63c3
Edmur <edmurneves@gmail.com>
2022-06-14 17:17:15 -0300

    Criação dos elementos do componente Login.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,13 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+import { createUser } from '../../services/userAPI';
+
+export default class Login extends React.Component {
+    state = {
+        formName: '',
+        loading: false,
+    };
 
     handleChangeForm = ({ target }) => {
         const { name, value } = target;
@@ -24,3 +34,41 @@
         );
     };
 
+    render() {
+        const { formName, loading } = this.state;
+
+        return loading ? (
+            <p> Carregando...</p>
+        ) : (            
+            <div>
+                <h1> My Tunes</h1>
+                <form>
+                    <label htmlFor="login-submit-input">
+                        Login:
+                        <input
+                            name="formName"
+                            id="login-submit-button"
+                            type="text"
+                            value={formName}
+                            placeholder="Nome"
+                            onChange={this.handleChangeForm}
+                        />
+                    </label>
+                    <button
+                        type="submit"
+                        onClick={this.onButtonClick}
+                        disabled={!this.buttonDisabled()}
+                    >
+                        Entrar
+                    </button>
+                </form>
+            </div>            
+        );
+    }
+}
+
+Login.propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func,
+    }).isRequired,
+  };
