cc77b4a059fe1207618a52429ca6c62901722178
Edmur <edmurneves@gmail.com>
2022-06-14 17:14:30 -0300

    Criação da função buttonDisabled para validar a entrada de dados do input.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -5,3 +5,10 @@
         this.setState({ [name]: value});
     };
 
+    buttonDisabled = () => {
+        const MIN_LENGTH_NAME = 3;
+        const { formName } = this.state;
+    
+        return formName.length >= MIN_LENGTH_NAME;
+      };
+
